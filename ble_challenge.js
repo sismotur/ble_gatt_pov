f07231f07b40f3963f75586534513b74325cbc14
Felipe Santi <sismotur.fsanti@gmail.com>
2017-05-13 10:48:04 -0200

    First commit

---
+++ ble_challenge.js
@@ -0,0 +1,94 @@
+/**
+ * SIMPLE GATT SERVER
+ * name: ble_challenge
+ * author: Felipe Santi
+ * copyright: Sismotur 2017 (C) All Rights Reserved
+ */
+
+// requires
+var bleno = require('bleno');
+var crypto = require('./crypto');
+
+// initial set up - max 21 chars due to BLE limit
+var _nonce_length = 21;
+
+// Advertise the BLE adrress when bleno start
+bleno.on('stateChange', function(state) {
+    console.log('State change: ' + state);
+        if(state === 'poweredOn') {
+        bleno.startAdvertising('RaspberryPi',['12ab']);
+        } else {
+        bleno.stopAdvertising();
+        }
+});
+
+// Log when accepting connections
+bleno.on('accept', function(clientAddress) {
+    console.log('Accepted connection from address: ' + clientAddress);
+});
+
+// Log when disconnecting
+bleno.on('disconnect', function(clientAddress) {
+    console.log('Disconnected from address: ' + clientAddress);
+});
+
+// Create a new service and characteristic when advertising begins
+bleno.on('advertisingStart', function(error) {
+    if(error) {
+        console.log("Advertising start error: " + error);
+    } else {
+        console.log("Advertising start success");
+        bleno.setServices([
+        // SERVICE PROOF OF VISIT
+            new bleno.PrimaryService({
+                uuid: 'ace0',
+            characteristics: [
+                // CHARACTERISTIC 1. Entry point - get a fresh random nonce
+                new bleno.Characteristic({
+                    value:null,
+                    uuid:'0001',
+                    properties: ['read'],
+                    descriptors: [
+                        new bleno.Descriptor({
+                            uuid: '0001',
+                            value: 'Proof of Visit entry: serves a nonce string'
+                        })
+                    ],
+                    // on read request, send a message back with the value
+                    onReadRequest: function(offset, callback) {
+                        var _nonce = crypto.nonce(_nonce_length);
+                        console.log("Proof of Visit read: nonce is: " + _nonce);
+                        callback(this.RESULT_SUCCESS, new Buffer(_nonce));
+                    }
+                }),
+                // CHARACTERISTIC 2. Receive public key, nonce, signature
+                new bleno.Characteristic({
+                    value:null,
+                    uuid:'0002',
+                    properties: ['write'],
+                    descriptors: [
+                        new bleno.Descriptor({
+                            uuid: '0001',
+                            value: 'Write here the challenge string'
+                        })
+                    ],
+                    // on write request, log in the console the value
+                    onWriteRequest: function(data, offset, withoutResponse, callback) {
+                        if(offset) {
+                            callback(this.RESULT_ATTR_NOT_LONG);
+                            console.log("Challenge string invalid (nil)");
+                        } else if (data.length < 4) {
+                            console.log("Challenge string invalid (min 4 characters)");
+                            callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
+                        } else {
+                            var _challenge = data.toString('utf-8');
+                            console.log("Challenge string trial: " + _challenge);
+                            callback(this.RESULT_SUCCESS);
+                        }
+                    }
+                })
+            ]
+        })
+        ])
+    }
+});
---
+++ crypto.js
@@ -0,0 +1,18 @@
+/**
+ * CRYPTO.JS
+ * author: Felipe Santi
+ * copyright: Sismotur (C) 2017
+ */
+
+var nonce = function(length) {
+    var _n = "";
+    var _chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < length; i++) {
+        // console.log("loop: " + i.toString());
+        _n += _chars.charAt(Math.floor(Math.random() * _chars.length));
+    }
+    console.log("crypto _n: " + _n.toString("utf-8"));
+    return _n.toString("utf-8");
+}
+
+module.exports.nonce  = nonce;
